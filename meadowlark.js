e17540b3e01022d9f94712770b2bd27c2bac615c
Teal Matrix <Tmatrix9977@conestogac.on.ca>
2017-03-22 11:35:55 -0400

    changed fortunecookie

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,7 @@
 var express = require('express');
 
 var app = express();
+var fortune = require('./lib/fortune.js');
 
 // set up handlebars view engine
 var handlebars = require('express-handlebars')
@@ -12,21 +13,12 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
-var fortuneCookies = [
-	"Conquer your fears or they will conquer you.",
-	"Rivers need springs.",
-	"Do not fear what you don't know.",
-	"You will have a pleasant surprise.",
-	"Whenever possible, keep it simple.",
-];
-
 app.get('/', function(req, res) {
 	res.render('home');
 });
 app.get('/about', function(req,res){
-	var randomFortune =
-		fortuneCookies[Math.floor(Math.random() * fortuneCookies.length)];
-	res.render('about', { fortune: randomFortune });
+
+	res.render('about', { fortune: fortune.getFortune() });
 });
 
 // 404 catch-all handler (middleware)
